4b95949029b5b7dc83953480b198039747a343f0
Somnath Ghule <ghulesomnath2111@gmail.com>
2026-06-15 00:04:26 +0530

    upadate

---
+++ src/main/java/com/deployment/deployment/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.deployment.deployment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "This is from CI/CD pipeline";
+    }
+}
